56c73f2a1c97b63e61626e7be9bce7a1afb4235c
JDL <16822206+JDL-84@users.noreply.github.com>
2020-09-09 22:34:55 +0100

    Update UK-Random-Postcode-Generator.ps1

--- UK-Random-Postcode-Generator.ps1
+++ UK-Random-Postcode-Generator.ps1
@@ -1,15 +1,16 @@
 ##https://github.com/JDL-84
-
+##
 Param(
-
     [Parameter(Position=1)]
-    [int]$Limiter = 1000
+    [int]$Limiter = 1000000
+
 )
 BEGIN
 {
 
     function GetRegion
     {
+          ##Wiki list of UK postcode regions
           $REGIONS = @( "AB"  , "AL"  , "B"  , "BA"  , "BB"  , "BD"  , "BH"  , "BL"  , "BN"  , "BR"  , "BS"  , "BT"  , "CA"  , "CB"  , "CF"  , "CH"  , "CM"  , "CO"  , "CR"  , "CT"  , "CV"  , "CW"  , "DA"  , "DD"  , "DE"  , "DG"  , "DH"  , "DL"  , "DN"  , "DT"  , "DY"  , "E"  , "EC"  , "EH"  , "EN"  , "EX"  , "FK"  , "FY"  , "G"  , "GL"  , "GU"  , "HA"  , "HD"  , "HG"  , "HP"  , "HR"  , "HS"  , "HU"  , "HX"  , "IG"  , "IP"  , "IV"  , "KA"  , "KT"  , "KW"  , "KY"  , "L"  , "LA"  , "LD"  , "LE"  , "LL"  , "LN"  , "LS"  , "LU"  , "M"  , "ME"  , "MK"  , "ML"  , "N"  , "NE"  , "NG"  , "NN"  , "NP"  , "NR"  , "NW"  , "OL"  , "OX"  , "PA"  , "PE"  , "PH"  , "PL"  , "PO"  , "PR"  , "RG"  , "RH"  , "RM"  , "S"  , "SA"  , "SE"  , "SG"  , "SK"  , "SL"  , "SM"  , "SN"  , "SO"  , "SP"  , "SR"  , "SS"  , "ST"  , "SW"  , "SY"  , "TA"  , "TD"  , "TF"  , "TN"  , "TQ"  , "TR"  , "TS"  , "TW"  , "UB"  , "W"  , "WA"  , "WC"  , "WD"  , "WF"  , "WN"  , "WR"  , "WS"  , "WV"  , "YO"  , "ZE"   )
           return Get-Random -InputObject $REGIONS 
     }
@@ -26,7 +27,19 @@ BEGIN
 
     function GetFakePostCode
     {
-          return ("{0}{1} {2}{3}{4}" -f (GetRegion),(GetNumber -min 1 -max 30),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter))
+        ##Random to Switch formats
+        switch((GetNumber -min 1 -max 4)) 
+        {
+          ##   AAnn nAA
+         1 {  return ("{0}{1} {2}{3}{4}" -f (GetRegion),(GetNumber -min 1 -max 99),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)) ; break}
+
+          ##   AAAn nAA
+         2 {  return ("{0}{1}{2} {3}{4}{5}" -f (GetRegion),(GetLetter),(GetNumber -min 0 -max 9),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)); break}
+
+          ##   AAnA nAA
+         3 {  return ("{0}{1}{2} {3}{4}{5}" -f (GetRegion),(GetNumber -min 0 -max 9),(GetLetter),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)); break}
+
+        }
     }
 
     $Round=0
@@ -34,9 +47,10 @@ BEGIN
     do
     {
         $Round++
-        GetFakePostCode 
+        GetFakePostCode #| Out-File -Append D:\Generated_PostCodes_UK_Unverified.log -Encoding utf8
  
     }
     while($Round -lt $Limiter)
 }
+
 
