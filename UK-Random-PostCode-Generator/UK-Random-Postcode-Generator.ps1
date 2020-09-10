3a9f80471af513847258283df1fae082a4e4e9de
JDL <16822206+JDL-84@users.noreply.github.com>
2020-09-10 08:21:34 +0100

    Set sticky region

--- UK-Random-PostCode-Generator/UK-Random-Postcode-Generator.ps1
+++ UK-Random-PostCode-Generator/UK-Random-Postcode-Generator.ps1
@@ -1,18 +1,49 @@
-##https://github.com/JDL-84
-##
+﻿<#
+.Synopsis
+   Random Unverified UK Post Code Generator
+.DESCRIPTION
+   Used to generate uk post codes for stress testing an internal address finder service. 
+.EXAMPLE
+   UK-Random-Postcode-Generator.ps1 -Limiter 1000
+   Generates 1000 random post codes
+.EXAMPLE
+   UK-Random-Postcode-Generator.ps1 -Limiter 1000 -REGION SE
+   Generates 1000 random post codes, for only SE region (SE## #AA)
+.EXAMPLE
+   UK-Random-Postcode-Generator.ps1 | Out-File -Append .\Generated_PostCodes_UK_Unverified.log -Encoding utf8
+   Generates 1000 random post codes,
+.LINK
+   https://github.com/JDL-84/Powershell/tree/master/UK-Random-PostCode-Generator
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
 Param(
     [Parameter(Position=1)]
-    [int]$Limiter = 1000000
+    [int]$LIMITER = 10000,
+    [Parameter(Position=1)]
+    [string]$REGION
 
 )
 BEGIN
 {
+    ##Wiki list of UK postcode regions
+    $REGIONS = @( "AB"  , "AL"  , "B"  , "BA"  , "BB"  , "BD"  , "BH"  , "BL"  , "BN"  , "BR"  , "BS"  , "BT"  , "CA"  , "CB"  , "CF"  , "CH"  , "CM"  , "CO"  , "CR"  , "CT"  , "CV"  , "CW"  , "DA"  , "DD"  , "DE"  , "DG"  , "DH"  , "DL"  , "DN"  , "DT"  , "DY"  , "E"  , "EC"  , "EH"  , "EN"  , "EX"  , "FK"  , "FY"  , "G"  , "GL"  , "GU"  , "HA"  , "HD"  , "HG"  , "HP"  , "HR"  , "HS"  , "HU"  , "HX"  , "IG"  , "IP"  , "IV"  , "KA"  , "KT"  , "KW"  , "KY"  , "L"  , "LA"  , "LD"  , "LE"  , "LL"  , "LN"  , "LS"  , "LU"  , "M"  , "ME"  , "MK"  , "ML"  , "N"  , "NE"  , "NG"  , "NN"  , "NP"  , "NR"  , "NW"  , "OL"  , "OX"  , "PA"  , "PE"  , "PH"  , "PL"  , "PO"  , "PR"  , "RG"  , "RH"  , "RM"  , "S"  , "SA"  , "SE"  , "SG"  , "SK"  , "SL"  , "SM"  , "SN"  , "SO"  , "SP"  , "SR"  , "SS"  , "ST"  , "SW"  , "SY"  , "TA"  , "TD"  , "TF"  , "TN"  , "TQ"  , "TR"  , "TS"  , "TW"  , "UB"  , "W"  , "WA"  , "WC"  , "WD"  , "WF"  , "WN"  , "WR"  , "WS"  , "WV"  , "YO"  , "ZE"   )
+    
+    #Set loop counter to 0
+    $ROUND=0
 
     function GetRegion
-    {
-          ##Wiki list of UK postcode regions
-          $REGIONS = @( "AB"  , "AL"  , "B"  , "BA"  , "BB"  , "BD"  , "BH"  , "BL"  , "BN"  , "BR"  , "BS"  , "BT"  , "CA"  , "CB"  , "CF"  , "CH"  , "CM"  , "CO"  , "CR"  , "CT"  , "CV"  , "CW"  , "DA"  , "DD"  , "DE"  , "DG"  , "DH"  , "DL"  , "DN"  , "DT"  , "DY"  , "E"  , "EC"  , "EH"  , "EN"  , "EX"  , "FK"  , "FY"  , "G"  , "GL"  , "GU"  , "HA"  , "HD"  , "HG"  , "HP"  , "HR"  , "HS"  , "HU"  , "HX"  , "IG"  , "IP"  , "IV"  , "KA"  , "KT"  , "KW"  , "KY"  , "L"  , "LA"  , "LD"  , "LE"  , "LL"  , "LN"  , "LS"  , "LU"  , "M"  , "ME"  , "MK"  , "ML"  , "N"  , "NE"  , "NG"  , "NN"  , "NP"  , "NR"  , "NW"  , "OL"  , "OX"  , "PA"  , "PE"  , "PH"  , "PL"  , "PO"  , "PR"  , "RG"  , "RH"  , "RM"  , "S"  , "SA"  , "SE"  , "SG"  , "SK"  , "SL"  , "SM"  , "SN"  , "SO"  , "SP"  , "SR"  , "SS"  , "ST"  , "SW"  , "SY"  , "TA"  , "TD"  , "TF"  , "TN"  , "TQ"  , "TR"  , "TS"  , "TW"  , "UB"  , "W"  , "WA"  , "WC"  , "WD"  , "WF"  , "WN"  , "WR"  , "WS"  , "WV"  , "YO"  , "ZE"   )
+    {        
           return Get-Random -InputObject $REGIONS 
+    }
+
+    function CheckPassedRegion
+    {
+        if(![String]::IsNullOrEmpty(($REGION))) 
+        { 
+            ##User set post code region. 
+            if(!$REGIONS.Contains($REGION.ToUpper())) { Write-Warning ("Supplied REGION '{0}' was not in list of known regions."-f $REGION)}
+        }
     }
 
     function GetLetter
@@ -27,31 +58,40 @@ BEGIN
 
     function GetFakePostCode
     {
+        Param([string]$SETREGION)        
+
+        ##Check if REGION was specified, if not geneerate one. 
+        if([String]::IsNullOrEmpty(($SETREGION))) { $SETREGION = (GetRegion) }
+      
+
         ##Random to Switch formats
         switch((GetNumber -min 1 -max 4)) 
         {
           ##   AAnn nAA
-         1 {  return ("{0}{1} {2}{3}{4}" -f (GetRegion),(GetNumber -min 1 -max 99),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)) ; break}
+         1 {  return ("{0}{1} {2}{3}{4}" -f $SETREGION,(GetNumber -min 1 -max 99),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)) ; break}
 
           ##   AAAn nAA
-         2 {  return ("{0}{1}{2} {3}{4}{5}" -f (GetRegion),(GetLetter),(GetNumber -min 0 -max 9),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)); break}
+         2 {  return ("{0}{1}{2} {3}{4}{5}" -f $SETREGION,(GetLetter),(GetNumber -min 0 -max 9),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)); break}
 
           ##   AAnA nAA
-         3 {  return ("{0}{1}{2} {3}{4}{5}" -f (GetRegion),(GetNumber -min 0 -max 9),(GetLetter),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)); break}
+         3 {  return ("{0}{1}{2} {3}{4}{5}" -f $SETREGION,(GetNumber -min 0 -max 9),(GetLetter),(GetNumber -min 0 -max 9),(GetLetter),(GetLetter)); break}
 
-        }
+        }        
+
     }
 
-    $Round=0
+    
+    ##Check if region parameter is in the list of known regions. 
+    ##Will just warn and allow it to continue. 
+    CheckPassedRegion
 
+    ##Genertate until we reach the limiter
     do
     {
-        $Round++
-        GetFakePostCode  #| Out-File -Append D:\Generated_PostCodes_UK_Unverified.log -Encoding utf8
-    }
-    while($Round -lt $Limiter)
-}
-
+        $ROUND++
+        GetFakePostCode -SETREGION $REGION
  
-
+    }
+    while( $ROUND -lt $LIMITER)
+}
 
